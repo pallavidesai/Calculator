4eb0d29a8aede0ed291ee07ae1bea4c5f9264beb
Pallavi Desai <s.pallavidesai@gmail.com>
2020-07-19 16:32:14 -0500

    Node JS simple time with web socket

---
+++ client.js
@@ -0,0 +1,8 @@
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
+var el;
+
+ws.onmessage = function (event) {
+  el = document.getElementById('server-time');
+  el.innerHTML = 'Server time: ' + event.data;
+};
