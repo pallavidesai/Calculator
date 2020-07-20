6d6899cbc3f5f7ad0313babf787df95c84cec022
Pallavi Desai <s.pallavidesai@gmail.com>
2020-07-19 23:22:59 -0500

    not working

--- frondend.js
+++ frondend.js
@@ -21,7 +21,7 @@ $(function () {
     return;
   }
   // open connection
-  var HOST = location.origin.replace(/^http/, 'ws');
+  var HOST = location.origin.replace(/^http/, '');
   var connection = new WebSocket(HOST);
   connection.onopen = function () {
     // first we want users to enter their names
